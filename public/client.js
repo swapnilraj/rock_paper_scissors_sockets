f9fba1a05866f3854a2ac8e9b7e6dcdfb315202f
Swapnil Raj <raj.swapnil99@gmail.com>
2017-01-16 13:48:01 +0530

    Added user selection through div clicks
    Added rock, paper and scissors to page with styling

--- public/client.js
+++ public/client.js
@@ -6,6 +6,15 @@ const button = document.getElementsByTagName('button')[0];
  * @type {HTMLInputElement}
  */
 const input = document.querySelector('body > input');
+/**
+ * @type {HTMLDivElement}
+ */
+const hands = document.querySelector('div.hands');
+const rps = {
+    'rock' : 1,
+    'paper': 2,
+    'scissors': 3,
+};
 let myTurn;
 const socket = io.connect('http://localhost:3000');
 
@@ -21,12 +30,15 @@ socket.on('opponent_played', (data) => {
         console.log('Draw');
     }
 });
-
-const playTurn = () => {  
-    console.log('Button Press');
-    myTurn = input.value;
+/**
+ * @param {Event} event
+ */
+const playTurn = (event) => {  
+    console.log(event.srcElement.alt);
+    myTurn = rps[event.srcElement.alt];
+    console.log(myTurn);
     socket.emit("user_played", myTurn);
 };
 
 ['click', 'touchstart'].forEach(event => button.addEventListener(event, playTurn));
-
+['click', 'touchstart'].forEach(event => hands.addEventListener(event, playTurn));
